911b4a15e3fb0f1d6f55ec6ecc6aef4d64c7bb8b
Sylvain Blondeau <sylvain@wildcodeschool.fr>
2020-06-02 10:30:39 +0200

    fix style animal show

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     .addEntry('home', './assets/scss/home.scss')
+    .addEntry('animal', './assets/scss/animal.scss')
     //.addEntry('page2', './assets/js/page2.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
