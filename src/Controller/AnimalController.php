00266fb1bb4cad17af3a0e961f9616c599a69678
Sylvain Blondeau <sylvain@wildcodeschool.fr>
2020-06-02 11:04:32 +0200

    list all animals

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -7,15 +7,30 @@ use App\Repository\AnimalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class AnimalController
+ * @package App\Controller
+ * @Route("/animal", name="animal_")
+ */
 class AnimalController extends AbstractController
 {
     /**
-     * @Route("/animal/details/{id}", name="animal_show")
+     * @Route("/details/{id}", name="show")
      */
     public function show(Animal $animal)
     {
         return $this->render('animal/show.html.twig', [
             'animal' => $animal,
+        ]);
+    }
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(AnimalRepository $animalRepository)
+    {
+        return $this->render('animal/index.html.twig', [
+            'animals' => $animalRepository->findBy([], ['name'=>'ASC']),
         ]);
     }
 }
